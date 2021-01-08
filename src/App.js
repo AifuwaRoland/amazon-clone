4efaa7fc16ed64ef90508f356619d3f45d84b86c
AifuwaRoland <siroapple10@gmail.com>
2021-01-07 21:18:33 -0600

    added header comp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-
+import Header from './Header';
 function App() {
   return (
     //BEM
     <div className="App">
-     <h1> Amazon Store</h1>
-
-     {/* Header */}
+    <Header/>
      {/* Home */}
-     
+
 
     </div>
   );
