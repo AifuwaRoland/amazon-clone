80516449d4ba7d555f1c913c285699e027437a40
AifuwaRoland <siroapple10@gmail.com>
2021-01-10 00:36:14 -0600

    added data layer

--- src/Header.js
+++ src/Header.js
@@ -4,13 +4,18 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+import {useStateValue} from "./StateProvider";
 function Header() {
+  const [{basket}, dispatch]= useStateValue();
   return (
     <div className="header">
-      <img
-        className="header_logo"
-        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      />
+      <Link to="/">
+        <img
+          className="header_logo"
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
 
       <div className="header_search">
         <input className="header_searchInput" type="text" />
@@ -30,10 +35,13 @@ function Header() {
           <span className="header_optionLineOne">Your</span>
           <span className="header_optionLineTwo">Prime</span>
         </div>
-        <div className="header_optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header_optionLineTwo header_basketCount">0</span>
-        </div>
+        <Link to="/checkout">
+          <div className="header_optionBasket">
+            <ShoppingBasketIcon />
+
+            <span className="header_optionLineTwo header_basketCount">{basket?.length}</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
