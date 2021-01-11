f31abc14cfa553b9d9ae16b3d41c0e36b40c780f
AifuwaRoland <siroapple10@gmail.com>
2021-01-10 19:58:51 -0600

    get subtotal of items n cart

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,32 +1,40 @@
-import React from 'react'
-import CurrencyFormat from "react-currency-format";
+import React from "react";
 import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
-    return (
-        <div className="subtotal">
-            <CurrencyFormat
+
+  const history = useHistory();
+  // eslint-disable-next-line 
+  const [{ basket }, dispatch] = useStateValue();
+
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-            {/* {basket.length} */}
-              Subtotal (0 items): <strong>{value}</strong>
+              
+              Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
-            <small className="subtotal__gift">
+            <small className="subtotal_gift">
               <input type="checkbox" /> This order contains a gift
             </small>
           </>
         )}
         decimalScale={2}
-        vale={0}
-        //value={getBasketTotal(basket)} 
+        value={getBasketTotal(basket)} 
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
       />
+
       <button>Proceed to Checkout</button>
-        </div>
-    )
+    </div>
+  );
 }
 
-export default Subtotal
+export default Subtotal;
