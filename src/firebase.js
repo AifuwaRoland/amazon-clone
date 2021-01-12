9ac795426631b789c8b2cedc755a053d2e6ba79b
AifuwaRoland <siroapple10@gmail.com>
2021-01-12 01:26:36 -0600

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,18 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCVnNXqrR7BsWSP25Gpf5EJKW2jjrtkP8Q",
-    authDomain: "clone-d7b25.firebaseapp.com",
-    projectId: "clone-d7b25",
-    storageBucket: "clone-d7b25.appspot.com",
-    messagingSenderId: "640042412579",
-    appId: "1:640042412579:web:a8cf781f48718c0cb89f96",
-    measurementId: "G-09ELPB6M86"
-  };
+  apiKey: "AIzaSyCVnNXqrR7BsWSP25Gpf5EJKW2jjrtkP8Q",
+  authDomain: "clone-d7b25.firebaseapp.com",
+  projectId: "clone-d7b25",
+  storageBucket: "clone-d7b25.appspot.com",
+  messagingSenderId: "640042412579",
+  appId: "1:640042412579:web:a8cf781f48718c0cb89f96",
+  measurementId: "G-09ELPB6M86",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db, auth};
