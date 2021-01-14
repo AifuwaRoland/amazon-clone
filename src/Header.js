f39f5dadd4a6a7c10957960bb7c1ad5ce66157bb
AifuwaRoland <siroapple10@gmail.com>
2021-01-13 21:21:31 -0600

    added orders link

--- src/Header.js
+++ src/Header.js
@@ -42,15 +42,17 @@ function Header() {
             </span>
           </div>
         </Link>
-
-        <div className="header_option">
-          <span className="header_optionLineOne">Returns</span>
-          <span className="header_optionLineTwo">Orders</span>
-        </div>
+        <Link to="/orders">
+          <div className="header_option">
+            <span className="header_optionLineOne">Returns</span>
+            <span className="header_optionLineTwo">Orders</span>
+          </div>
+        </Link>
         <div className="header_option">
           <span className="header_optionLineOne">Your</span>
           <span className="header_optionLineTwo">Prime</span>
         </div>
+
         <Link to="/checkout">
           <div className="header_optionBasket">
             <ShoppingBasketIcon />
